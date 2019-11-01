52e8a3746521cb560214412d8c493796e3dcef23
Liam McKay <lmckay@msg.ucsf.edu>
2019-11-01 10:59:30 -0700

    initial commit

---
+++ bearbuy_scrape.py
@@ -0,0 +1,83 @@
+from bs4 import BeautifulSoup
+import copy
+import os
+import sys
+import json as js
+import csv
+import glob 
+
+def match_class(target):
+    def do_match(tag):
+        try:
+            classes = dict(tag.attrs)["class"]
+        except KeyError:
+            classes = ""
+        return all(c in classes for c in target)
+    return do_match
+
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#navigating-the-tree
+def has_class(tag):
+    return tag.has_attr('class')
+
+
+def main():
+    html_files = glob.glob("*.html")
+    output_tsv = ""
+    for file_name in html_files:
+        with open(file_name, "r") as html_doc:
+            soup = BeautifulSoup("".join(html_doc.readlines()), 'html.parser')
+
+            company_boxes = soup.find(['div', 'table', 'tbody', 'tr', 'td', 'div', 'div', 'div', 'table', 'tbody', 'tr', 'td'], recursive=True, attrs="ForegroundPanel")
+            company_boxes = soup.find('td', attrs="ForegroundPanel")
+            # print(mettlerToledo)
+            # print([a for a in company_boxes.contents[1].children])
+            findmysiblings = copy.copy(company_boxes.div.div.find('a', 'SupplierName'))
+
+            json = {}
+            for child in company_boxes.div.div.children:
+                splitRows=False
+                company = child.find("a")
+                if child.find("a") != -1:
+                    if "Subtotal" in company.string:
+                        continue
+                    if "Hide line details" not in company.string:
+                        json[company.string] = {}
+                        row = []
+                        for i,td in enumerate(child.find_all("td", "LineSixPack")):
+                            for a in td.stripped_strings:
+                                if "more info..." not in a:
+                                    row.append(a.replace("\xa0",'').replace('USD','').replace('\n','/'))
+                            if i > 10:
+                                splitRows=True
+                        if splitRows:
+                            list_rows = []
+                            indices = [0]
+                            for i,item in enumerate(row):
+                                if "addLineData" in item:
+                                    indices.append(i+1)
+                            for i in range(len(indices)-1):
+                                list_rows.append(row[indices[i]:indices[i+1]-1])
+                            if list_rows:
+                                json[company.string]={row[0]:row[1:] for row in list_rows}
+                        else:
+                            if row:
+                                json[company.string]={row[0]:row[1:-1]}
+
+        with open("data"+file_name.replace("html",'json'), 'w') as json_file_out:
+            js.dump(json, json_file_out)
+
+        for company, row in json.items():
+            for number, item in row.items():
+                output_tsv += file_name+"\t"+file_name.replace("Summary - Requisition ","").replace(".html","")+"\t"+company+"\t"+number+"\t"
+                output_tsv += item[0]+"\t"
+                if "/EA" not in item[1]:
+                    output_tsv += item[1]+"\t"
+                else:
+                    output_tsv += "\t"
+                output_tsv += item[-1]+"\t"
+                output_tsv += "\n"
+    with open("allRequisitions.tsv", 'w') as tsvout:
+        tsvout.write("Requisition\tRequisition Number\tCompany\tNumber\tItem Description\tCatalog Number\tTotal Price\n")
+        tsvout.write(output_tsv)
+
+main()

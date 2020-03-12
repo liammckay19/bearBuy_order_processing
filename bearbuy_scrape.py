b6382ece58dce647b7d399c72e8d5e31145cc133
Liam McKay <lmckay@msg.ucsf.edu>
2020-03-12 13:25:25 -0700

    date of req completed

--- bearbuy_scrape.py
+++ bearbuy_scrape.py
@@ -26,14 +26,27 @@ def main():
     html_files = glob.glob("*.html")
     output_tsv = ""
     for file_name in html_files:
+        print(file_name)
         with open(file_name, "r") as html_doc:
             soup = BeautifulSoup("".join(html_doc.readlines()), 'html.parser')
 
             # the magic location of the info (hopefully BearBuy doesn't update their HTML soon)
-            company_boxes = soup.find(['div', 'table', 'tbody', 'tr', 'td', 'div', 'div', 'div', 'table', 'tbody', 'tr', 'td'], recursive=True, attrs="ForegroundPanel")
+            company_boxes = soup.find(['div', 'table', 'tbody', 'tr', 'td', 'div', 'div', 
+                'div', 'table', 'tbody', 'tr', 'td'], recursive=True, attrs="ForegroundPanel")
+            
+            try:
+                date_completed = soup.find(['td','div','table','tbody','td','div'], recursive=True, attrs=["TabbedBackgroundPanel","tabpanel"])
+                # print([a for a in date_completed.contents[3].table.tbody.children])
+                date_completed_further = [a.find('div') for a in date_completed.contents[3].table.tbody.children][4]
+                date = [a.find('div') for a in date_completed_further.table.tbody.children][2]
+                date_str = date.div.string.rstrip().replace("\n",'').replace('(','').replace(')','')
+            except AttributeError as e:
+                print("Couldn't find date for "+file_name)
+                date_str=''
+                continue
+
             # company_boxes = soup.find('td', attrs="ForegroundPanel")
             # print(mettlerToledo)
-            # print([a for a in company_boxes.contents[1].children])
             try:
                 findmysiblings = copy.copy(company_boxes.div.div.find('a', 'SupplierName'))
             except AttributeError as e:
@@ -50,7 +63,7 @@ def main():
                     if "Hide line details" not in company.string:
                         json[company.string] = {}
                         row = []
-                        for i,td in enumerate(child.find_all("td", "LineSixPack")):
+                        for i,td in enumerate(child.find_all("td", "LineSixPack")): # item bought information
                             for a in td.stripped_strings:
                                 if "more info..." not in a:
                                     row.append(a.replace("\xa0",'').replace('USD','').replace('\n','/'))
@@ -69,19 +82,13 @@ def main():
                         else:
                             if row:
                                 json[company.string]={row[0]:row[1:-1]}
-                        print(row[:5])
+                        # print(row)
                         
-
-        with open("data"+file_name.replace("html",'json'), 'w') as json_file_out:
-            js.dump(json, json_file_out)
-        if json:
-            print("wrote to "+"data"+file_name.replace("html",'json'))
-        else:
-            print("json is empty for "+"data"+file_name.replace("html",'json'))
 
 
         # add data to output_tsv string
         for company, row in json.items():
+            print(json.items())
             for number, item in row.items():
                 output_tsv += file_name+"\t"+file_name.replace("Summary - Requisition ","").replace(".html","")+"\t"+company+"\t"+number+"\t"
                 output_tsv += item[0]+"\t"
@@ -94,8 +101,19 @@ def main():
                 output_tsv += item[-3]+"\t"
                 output_tsv += item[-2]+"\t"
                 output_tsv += item[-1]+"\t"
+                output_tsv += date_str+"\t"
                 output_tsv += "\n"
-    
+
+        json['Date']=date_str
+
+        with open("data"+file_name.replace("html",'json'), 'w') as json_file_out:
+            js.dump(json, json_file_out)
+        if json:
+            print("wrote to "+"data"+file_name.replace("html",'json'))
+        else:
+            print("json is empty for "+"data"+file_name.replace("html",'json'))
+
+
     # output_tsv to allRequisitions.tsv
     with open("allRequisitions.tsv", 'w') as tsvout:
         tsvout.write("Requisition\tRequisition Number\tCompany\tNumber\tItem Description\tCatalog Number\tSize / Packaging\tUnit Price\tQuantity\tExt. Price\n")
